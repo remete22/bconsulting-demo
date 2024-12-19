020539e1a4ae8aa345c0ee552eea4eb46c901019
Kereszturi Ákos <kereszturi.akos@netpeople.hu>
2024-12-19 12:24:00 +0100

    solutions section

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -32,9 +32,23 @@ window.addEventListener('scroll', () => {
 const heroSlider = new Splide('.hero__slider', {
   type: 'loop',
   perPage: 1,
+  autoplay: true,
+  interval: 5000,
   pauseOnHover: true,
   arrows: false,
   pagination: true
 })
 
 heroSlider.mount()
+
+const solutionsSlider = new Splide('.solutions__slider', {
+  type: 'loop',
+  perPage: 3,
+  perMove: 1,
+  gap: 60,
+  arrows: true,
+  arrowPath: "M21.6161 6.61612C22.1043 6.12796 22.8957 6.12796 23.3839 6.61612L35.8839 19.1161C36.1183 19.3505 36.25 19.6685 36.25 20C36.25 20.3315 36.1183 20.6495 35.8839 20.8839L23.3839 33.3839C22.8957 33.872 22.1043 33.872 21.6161 33.3839C21.128 32.8957 21.128 32.1043 21.6161 31.6161L31.9822 21.25H5C4.30964 21.25 3.75 20.6904 3.75 20C3.75 19.3096 4.30964 18.75 5 18.75H31.9822L21.6161 8.38388C21.128 7.89573 21.128 7.10427 21.6161 6.61612Z",
+  pagination: false
+})
+
+solutionsSlider.mount()
